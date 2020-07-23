92c24f2b1122a7596a6eb2abbc4a7aa1f1188fa6
Cristian Franco <cfrancobedoya@gmail.com>
2020-07-23 01:57:05 -0500

    Adding video player.

---
+++ client/src/assets/js/mediaplayer.js
@@ -0,0 +1,36 @@
+function MediaPlayer(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
+};
+
+
+
+MediaPlayer.prototype._initPlugins = function() {
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        get muted() {
+            return this.media.muted;
+        },
+
+        set muted(value) {
+            this.media.muted = value;
+        }
+    };
+
+    this.plugins.forEach(plugin => {
+        plugin.run(player);
+    })
+};
+
+MediaPlayer.prototype.play = function () {
+    (this.media.paused) ? this.media.play() : this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+    (this.media.muted) ? this.media.muted = false : this.media.muted = true;
+};
+
+export default MediaPlayer;
