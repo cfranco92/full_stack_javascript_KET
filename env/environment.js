2d5302db52be88c57b295120d62fc8bc48cda15d
Cristian Franco <cfrancobedoya@gmail.com>
2020-07-23 19:00:54 -0500

    Root path

--- env/environment.js
+++ env/environment.js
@@ -2,9 +2,9 @@ const environment = {
     dbUrl: process.env.DB_URL || 'mongodb://cristian:UlRY53hxJueBpSiK@cluster0-shard-00-00-ksmmt.gcp.mongodb.net:27017,cluster0-shard-00-01-ksmmt.gcp.mongodb.net:27017,cluster0-shard-00-02-ksmmt.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'http://localhost',
-    publicRoute: process.env.PUBLIC_ROUTE || './../wwwroot/full_stack_javascript_KET/client/dist/',
+    publicRoute: process.env.PUBLIC_ROUTE || './../full_stack_javascript_KET/client/dist/',
     filesRoute: process.env.FILES_ROUTE || 'files',
-    angularIndex: './../wwwroot/full_stack_javascript_KET/client/dist/client/index.html'
+    angularIndex: './../full_stack_javascript_KET/client/dist/client/index.html'
 };
 
 module.exports = environment;
